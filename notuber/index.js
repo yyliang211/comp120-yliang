30faed408b3151d2ab458527624a04cc3ba78df3
Yingyang Liang <yingyang@YYs-MacBook-Pro.local>
2021-06-11 21:01:57 -0400

    first commit

---
+++ notuber/index.js
@@ -0,0 +1,8 @@
+let map;
+
+function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+                center: {lat: 42.352271, lng: -71.05524200000001},
+                zoom: 14,
+        });
+}
